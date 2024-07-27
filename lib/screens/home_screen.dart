318278b5914acf7768a702a3ff19f2c1d142d8f4
davitAI <khanaldavit@gmail.com>
2024-07-27 22:46:46 +0545

    updated names in test

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+// import 'package:intl/intl.dart';
 import 'add_transaction_screen.dart';
 import '../models/transaction.dart';
 import '../widgets/transaction_list.dart';
